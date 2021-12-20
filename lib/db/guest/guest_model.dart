98542f4c641aedffcfff559bbfd9a8483b2c32a5
mohamadbastin <mohamadbastin@gmail.com>
2021-12-20 19:45:37 +0330

    guest model

---
+++ lib/db/guest/guest_model.dart
@@ -0,0 +1,34 @@
+import 'package:floor/floor.dart';
+
+@entity
+class Guest {
+  Guest(
+      {required this.name,
+      required this.nationalId,
+      this.gender,
+      required this.address,
+      required this.birthDate,
+      required this.password,
+      required this.phoneNumber});
+
+  @PrimaryKey(autoGenerate: true)
+  int? id;
+  String name;
+  String nationalId;
+  String? gender;
+  String address;
+  String birthDate;
+  String password;
+  String phoneNumber;
+}
+
+
+// 'CREATE TABLE IF NOT EXISTS `Guest`
+// (`id` INTEGER PRIMARY KEY AUTOINCREMENT,
+// `name` TEXT NOT NULL,
+// `nationalId` TEXT NOT NULL,
+// `gender` TEXT,
+// `address` TEXT NOT NULL,
+// `birthDate` TEXT NOT NULL,
+// `password` TEXT NOT NULL,
+// `phoneNumber` TEXT NOT NULL)'
