e9a273f0b74410d9a31248ac8419f46500b10ed6
mohamadbastin <mohamadbastin@gmail.com>
2021-12-20 19:46:46 +0330

    restaurant model

---
+++ lib/db/resturant_coffee_shop/resturant_coffeeshop_model.dart
@@ -0,0 +1,16 @@
+import 'package:floor/floor.dart';
+
+@entity
+class RestaurantCoffeeShop {
+  RestaurantCoffeeShop({required this.name, required this.type});
+
+  @PrimaryKey(autoGenerate: true)
+  int? id;
+  String name;
+  int type; //0: restaurant - 1: coffee shop
+}
+
+// 'CREATE TABLE IF NOT EXISTS `RestaurantCoffeeShop`
+// (`id` INTEGER PRIMARY KEY AUTOINCREMENT,
+// `name` TEXT NOT NULL,
+// `type` INTEGER NOT NULL)'
