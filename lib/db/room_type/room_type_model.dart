31923ff73d3d286098147ddcd7e7a18f49b53078
mohamadbastin <mohamadbastin@gmail.com>
2021-12-20 19:47:53 +0330

    room type model

---
+++ lib/db/room_type/room_type_model.dart
@@ -0,0 +1,26 @@
+import 'package:floor/floor.dart';
+
+const String desc = "this a base description for the room.";
+
+@entity
+class RoomType {
+  RoomType(
+      {required this.name,
+      required this.numberOfBeds,
+      required this.image,
+      this.description = desc});
+
+  @PrimaryKey(autoGenerate: true)
+  int? id;
+  String name;
+  int numberOfBeds;
+  String image;
+  String description;
+}
+
+// 'CREATE TABLE IF NOT EXISTS `RoomType`
+// (`id` INTEGER PRIMARY KEY AUTOINCREMENT,
+// `name` TEXT NOT NULL,
+// `numberOfBeds` INTEGER NOT NULL,
+// `image` TEXT NOT NULL,
+// `description` TEXT NOT NULL)'
