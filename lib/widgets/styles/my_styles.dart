ba7bea3f426a8fbe8fad9f8b2a6a3db8e6466531
mohamadbastin <mohamadbastin@gmail.com>
2021-12-26 11:01:31 +0330

    my styles

---
+++ lib/widgets/styles/my_styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MyStyles {
+  static TextStyle bigBoldText36 =
+      const TextStyle(fontWeight: FontWeight.bold, fontSize: 36);
+  static TextStyle bigText28 =
+      const TextStyle(fontWeight: FontWeight.w500, fontSize: 28);
+  static TextStyle normalText20 = const TextStyle(fontSize: 20);
+}
