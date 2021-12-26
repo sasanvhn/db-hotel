a7cf253dd3d3f252d9ec55ca5c81fe0e0acf879e
mohamadbastin <mohamadbastin@gmail.com>
2021-12-26 11:09:37 +0330

    first screen

---
+++ lib/screens/first_screen/first_screen.dart
@@ -0,0 +1,64 @@
+import 'package:db_hotel/widgets/styles/my_styles.dart';
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  _FirstScreenState createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: null,
+        title: const Text("Hotel DB Project"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Choose an actor",
+              style: MyStyles.bigBoldText36,
+            ),
+            Wrap(
+              spacing: 20,
+              runSpacing: 20,
+              children: [
+                Container(
+                  width: 300,
+                  height: 100,
+                  decoration: MyStyles.roundedBox,
+                  child: const Center(
+                    child: Text("User"),
+                  ),
+                ),
+                Container(
+                  width: 300,
+                  height: 100,
+                  decoration: MyStyles.roundedBox,
+                  child: const Center(
+                    child: Text("Staff"),
+                  ),
+                ),
+                Container(
+                  width: 300,
+                  height: 100,
+                  decoration: MyStyles.roundedBox,
+                  child: const Center(
+                    child: Text("Admin"),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
