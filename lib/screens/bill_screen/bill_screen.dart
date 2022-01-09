5df15fc716fc1f84d8bea46ffd827cce9566f285
mohamadbastin <mohamadbastin@gmail.com>
2022-01-09 14:15:07 +0330

    fix bugz

--- lib/screens/bill_screen/bill_screen.dart
+++ lib/screens/bill_screen/bill_screen.dart
@@ -43,36 +43,74 @@ class BillScreen extends StatelessWidget {
                     return const CircularProgressIndicator();
                   }
                   if (snapshot.connectionState == ConnectionState.done) {
-                    for (ReservationDetails rd in snapshot.data!) {
-                      return FutureBuilder(
-                        future: _getDetails(rd),
-                        builder: (context,
-                            AsyncSnapshot<Map<String, int>> snapshot) {
-                          if (snapshot.connectionState ==
-                              ConnectionState.waiting) {
-                            return const CircularProgressIndicator();
-                          }
-                          if (snapshot.connectionState ==
-                              ConnectionState.done) {
-                            // sum = sum +
-                            //     int.parse(snapshot.data!["room"].toString());
-                            // sum = sum +
-                            //     int.parse(snapshot.data!["food"].toString());
-                            return Column(
-                              children: [
-                                Text("for room #${rd.room}:"),
-                                Padding(
-                                  padding: const EdgeInsets.only(left: 18.0),
-                                  child: Text(
-                                      "room: ${snapshot.data!["room"]}, food: ${snapshot.data!["food"]}"),
-                                )
-                              ],
-                            );
-                          }
-                          return Container();
-                        },
-                      );
-                    }
+                    log(snapshot.data!.length.toString());
+                    int sum = 0;
+                    return Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: List.generate(snapshot.data!.length, (index) {
+                          return FutureBuilder(
+                            future: _getDetails(snapshot.data![index]),
+                            builder: (context,
+                                AsyncSnapshot<Map<String, int>> snapshot2) {
+                              if (snapshot2.connectionState ==
+                                  ConnectionState.waiting) {
+                                return const CircularProgressIndicator();
+                              }
+                              if (snapshot2.connectionState ==
+                                  ConnectionState.done) {
+                                sum = snapshot2.data!["room"]! + sum;
+                                sum = snapshot2.data!["food"]! + sum;
+                                return Column(
+                                  children: [
+                                    Text(
+                                        "for room #${snapshot.data![index].room}:"),
+                                    Padding(
+                                      padding:
+                                          const EdgeInsets.only(left: 108.0),
+                                      child: Text(
+                                          "room: ${snapshot2.data!["room"]}, food: ${snapshot2.data!["food"]}"),
+                                    ),
+                                    const SizedBox(
+                                      height: 30,
+                                    )
+                                  ],
+                                );
+                              }
+                              return Container();
+                            },
+                          );
+                        }));
+                    // for (ReservationDetails rd in snapshot.data!) {
+                    //   log(rd.toString());
+                    //   return FutureBuilder(
+                    //     future: _getDetails(rd),
+                    //     builder: (context,
+                    //         AsyncSnapshot<Map<String, int>> snapshot) {
+                    //       if (snapshot.connectionState ==
+                    //           ConnectionState.waiting) {
+                    //         return const CircularProgressIndicator();
+                    //       }
+                    //       if (snapshot.connectionState ==
+                    //           ConnectionState.done) {
+                    //         // sum = sum +
+                    //         //     int.parse(snapshot.data!["room"].toString());
+                    //         // sum = sum +
+                    //         //     int.parse(snapshot.data!["food"].toString());
+                    //         return Column(
+                    //           children: [
+                    //             Text("for room #${rd.room}:"),
+                    //             Padding(
+                    //               padding: const EdgeInsets.only(left: 18.0),
+                    //               child: Text(
+                    //                   "room: ${snapshot.data!["room"]}, food: ${snapshot.data!["food"]}"),
+                    //             )
+                    //           ],
+                    //         );
+                    //       }
+                    //       return Container();
+                    //     },
+                    //   );
+                    // }
                   }
                   return Container();
                 },
@@ -102,7 +140,7 @@ class BillScreen extends StatelessWidget {
   Future<List<ReservationDetails>> _getReservationDetails() async {
     final List<ReservationDetails> rds =
         await database.reservationDetailDao.getRoomsIDsByResID(reservationID);
-
+    log(rds.toString());
     return rds;
   }
 
